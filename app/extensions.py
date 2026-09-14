fd75bdaf113e36d56430d3d276608c53ed09a392
Katakam-Krupavathi <krupavathikatakam2006@gmail.com>
2026-09-14 11:17:24 +0530

    feat: initialize Flask-Limiter for request rate limiting

--- app/extensions.py
+++ app/extensions.py
@@ -1,11 +1,14 @@
-from flask_sqlalchemy import SQLAlchemy
+﻿from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from flask_mail import Mail
 from flasgger import Swagger
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 db = SQLAlchemy()
 migrate = Migrate()
 jwt = JWTManager()
 mail = Mail()
 swagger = Swagger()
+limiter = Limiter(key_func=get_remote_address, storage_uri="memory://")
